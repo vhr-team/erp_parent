efea47b3481042ca5cad2f9a59474e37e08c157c
xhh <303158131@qq.com>
2020-04-13 23:17:02 +0800

    添加依赖

--- erp_permissions/src/main/java/cn/ddossec/PermissionsApp.java
+++ erp_permissions/src/main/java/cn/ddossec/PermissionsApp.java
@@ -1,10 +1,10 @@
 package cn.ddossec;
 
 /**
- * @author 30315
+ * @author 唐疑豪
  * @title: PermissionsApp
  * @projectName erp_parent
- * @description: TODO
+ * @description: 权限模块
  * @date 2020-04-1321:16
  */
 public class PermissionsApp {

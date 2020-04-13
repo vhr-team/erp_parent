86d2928a3d8102e65dd7cd1939f437e2a267acba
xhh <303158131@qq.com>
2020-04-13 21:16:23 +0800

    添加权限模块的启动类

---
+++ erp_permissions/src/main/java/cn/ddossec/PermissionsApp.java
@@ -0,0 +1,11 @@
+package cn.ddossec;
+
+/**
+ * @author 30315
+ * @title: PermissionsApp
+ * @projectName erp_parent
+ * @description: TODO
+ * @date 2020-04-1321:16
+ */
+public class PermissionsApp {
+}

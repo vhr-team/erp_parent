0ff1b8785ceae76b39c0169ed149ab6bd4822a93
xhh <303158131@qq.com>
2020-04-16 10:28:30 +0800

    解决已登陆访问需要授权的资源返回json删除

---
+++ erp_permissions/src/main/java/cn/ddossec/shiro/GlobalExceptionHanderAdvise.java
@@ -0,0 +1,22 @@
+package cn.ddossec.shiro;
+
+import org.apache.shiro.authz.UnauthorizedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHanderAdvise {
+    /**
+     * 未授权
+     */
+    @ExceptionHandler(value = {UnauthorizedException.class})
+    public Object unauthorized() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", -1);
+        map.put("msg", "未授权，请联系管理员");
+        return map;
+    }
+}

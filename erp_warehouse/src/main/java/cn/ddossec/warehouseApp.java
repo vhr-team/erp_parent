c9ac489e75ac636fcfd8fb5607be5b466928e772
dell <1744611518@qq.com>
2020-04-17 12:39:34 +0800

    仓库管理

---
+++ erp_warehouse/src/main/java/cn/ddossec/warehouseApp.java
@@ -0,0 +1,12 @@
+package cn.ddossec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class warehouseApp {
+    public static void main(String[] args) {
+        SpringApplication.run(warehouseApp.class,args);
+    }
+}

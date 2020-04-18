33be0654a4a369ac72eea9667f5d9ac675c9d534
xhh <303158131@qq.com>
2020-04-18 17:45:25 +0800

    添加监听器

---
+++ erp_permissions/src/main/java/cn/ddossec/listener/TaskListenerImpl.java
@@ -0,0 +1,18 @@
+package cn.ddossec.listener;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+/**
+ * @author 30315
+ * @title: TaskListenerImpl
+ * @projectName erp_parent
+ * @description: TODO
+ * @date 2020-04-1817:44
+ */
+public class TaskListenerImpl implements TaskListener {
+    @Override
+    public void notify(DelegateTask delegateTask) {
+
+    }
+}

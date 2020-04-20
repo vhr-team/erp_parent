7f1cb617cf1a632bcffef9bb6e99ad96352c2598
xhh <303158131@qq.com>
2020-04-20 17:28:07 +0800

    监听，设置任务领导办理人

--- erp_permissions/src/main/java/cn/ddossec/listener/TaskListenerImpl.java
+++ erp_permissions/src/main/java/cn/ddossec/listener/TaskListenerImpl.java
@@ -1,5 +1,9 @@
 package cn.ddossec.listener;
 
+import cn.ddossec.common.AppUtils;
+import cn.ddossec.common.toolUtils;
+import cn.ddossec.domain.User;
+import cn.ddossec.service.UserService;
 import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.delegate.TaskListener;
 
@@ -11,8 +15,20 @@ import org.activiti.engine.delegate.TaskListener;
  * @date 2020-04-1817:44
  */
 public class TaskListenerImpl implements TaskListener {
+
+    private static final long serialVersionUID = 1L;
+
     @Override
     public void notify(DelegateTask delegateTask) {
-
+        //得到当前用户
+        User currentUser = toolUtils.getCurrentUser();
+        //取出领导ID
+        Integer mgr = currentUser.getMgr();
+        //从IOC容器里面取出UserService
+        UserService userService = AppUtils.getContext().getBean(UserService.class);
+        //3查询领导信息
+        User leaderUser = userService.queryUserById(mgr);
+        //4,设置办理人
+        delegateTask.setAssignee(leaderUser.getName());
     }
 }

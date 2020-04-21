1a745da4dd966ed88821e23313dcdc709360f252
xhh <303158131@qq.com>
2020-04-21 11:44:31 +0800

    impl NoticeService

--- erp_permissions/src/main/java/cn/ddossec/service/impl/NoticeServiceImpl.java
+++ erp_permissions/src/main/java/cn/ddossec/service/impl/NoticeServiceImpl.java
@@ -1,20 +1,41 @@
 package cn.ddossec.service.impl;
 
-import org.springframework.stereotype.Service;
-import javax.annotation.Resource;
-import java.util.List;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import cn.ddossec.mapper.NoticeMapper;
+import cn.ddossec.common.DataGridView;
 import cn.ddossec.domain.Notice;
+import cn.ddossec.mapper.NoticeMapper;
 import cn.ddossec.service.NoticeService;
-/**  
-    * @title: NoticeServiceImpl
-    * @projectName erp_parent
-    * @description: TODO
-    * @author 30315
-    * @date 2020-04-2111:42
-    */
-@Service
-public class NoticeServiceImpl extends ServiceImpl<NoticeMapper, Notice> implements NoticeService{
+import cn.ddossec.vo.NoticeVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+/**
+ * @author 30315
+ * @title: NoticeServiceImpl
+ * @projectName erp_parent
+ * @description: TODO
+ * @date 2020-04-2111:42
+ */
+@Service
+public class NoticeServiceImpl extends ServiceImpl<NoticeMapper, Notice> implements NoticeService {
+
+    @Autowired
+    private NoticeMapper noticeMapper;
+
+    @Override
+    public DataGridView queryAllNotice(NoticeVo noticeVo) {
+        IPage<Notice> page = new Page<>(noticeVo.getPage(), noticeVo.getLimit());
+        QueryWrapper<Notice> qw = new QueryWrapper<>();
+        qw.like(StringUtils.isNotBlank(noticeVo.getTitle()), "title", noticeVo.getTitle());
+        qw.like(StringUtils.isNotBlank(noticeVo.getOpername()), "opername", noticeVo.getOpername());
+        qw.ge(noticeVo.getStartTime() != null, "createtime", noticeVo.getStartTime());
+        qw.le(noticeVo.getEndTime() != null, "createtime", noticeVo.getEndTime());
+        qw.orderByDesc("createtime");
+        this.noticeMapper.selectPage(page, qw);
+        return new DataGridView(page.getTotal(), page.getRecords());
+    }
 }

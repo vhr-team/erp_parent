2750313b83f6978ed288ca90097779095293b18d
xhh <303158131@qq.com>
2020-04-21 10:27:18 +0800

    添加LoginfoController

---
+++ erp_permissions/src/main/java/cn/ddossec/controller/LoginfoController.java
@@ -0,0 +1,11 @@
+package cn.ddossec.controller;
+
+/**
+ * @author 30315
+ * @title: LoginfoController
+ * @projectName erp_parent
+ * @description: TODO
+ * @date 2020-04-2110:27
+ */
+public class LoginfoController {
+}

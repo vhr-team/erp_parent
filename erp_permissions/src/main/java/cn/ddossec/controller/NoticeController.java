a982e9ed47a607f522a6dbf39f8b674808462d9f
xhh <303158131@qq.com>
2020-04-21 11:38:33 +0800

    创建NoticeController

---
+++ erp_permissions/src/main/java/cn/ddossec/controller/NoticeController.java
@@ -0,0 +1,11 @@
+package cn.ddossec.controller;
+
+/**
+ * @author 30315
+ * @title: NoticeController
+ * @projectName erp_parent
+ * @description: TODO
+ * @date 2020-04-2111:38
+ */
+public class NoticeController {
+}

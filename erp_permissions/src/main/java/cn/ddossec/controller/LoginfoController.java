c01c0965d4e079ca3568461a82b12153bdfe3afe
xhh <303158131@qq.com>
2020-04-21 10:30:29 +0800

    查询所有登录日志

--- erp_permissions/src/main/java/cn/ddossec/controller/LoginfoController.java
+++ erp_permissions/src/main/java/cn/ddossec/controller/LoginfoController.java
@@ -1,5 +1,13 @@
 package cn.ddossec.controller;
 
+import cn.ddossec.common.DataGridView;
+import cn.ddossec.service.LoginfoService;
+import cn.ddossec.vo.LoginfoVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author 30315
  * @title: LoginfoController
@@ -7,5 +15,21 @@ package cn.ddossec.controller;
  * @description: TODO
  * @date 2020-04-2110:27
  */
+@RestController
+@RequestMapping("loginfo")
 public class LoginfoController {
+
+    @Autowired
+    private LoginfoService loginfoService;
+
+    /**
+     * 查询所有登录日志
+     * @param loginfoVo
+     * @return
+     */
+    @GetMapping("loadAllLoginfo")
+    public DataGridView loadAllLoginfo(LoginfoVo loginfoVo) {
+        return this.loginfoService.queryAllLoginfo(loginfoVo);
+    }
+
 }

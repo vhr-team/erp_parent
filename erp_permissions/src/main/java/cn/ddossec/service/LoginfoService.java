aece52c5eef645b433e46f280a71b6aebe404482
xhh <303158131@qq.com>
2020-04-21 10:30:43 +0800

    查询所有登录日志

--- erp_permissions/src/main/java/cn/ddossec/service/LoginfoService.java
+++ erp_permissions/src/main/java/cn/ddossec/service/LoginfoService.java
@@ -1,15 +1,23 @@
 package cn.ddossec.service;
 
+import cn.ddossec.common.DataGridView;
 import cn.ddossec.domain.Loginfo;
+import cn.ddossec.vo.LoginfoVo;
 import com.baomidou.mybatisplus.extension.service.IService;
-    /**  
-    * @title: LoginfoService
-    * @projectName erp_parent
-    * @description: TODO
-    * @author 30315
-    * @date 2020-04-2110:24
-    */
-public interface LoginfoService extends IService<Loginfo>{
 
+/**
+ * @author 30315
+ * @title: LoginfoService
+ * @projectName erp_parent
+ * @description: TODO
+ * @date 2020-04-2110:24
+ */
+public interface LoginfoService extends IService<Loginfo> {
 
+    /**
+     * 查询所有登录日志
+     * @param loginfoVo
+     * @return
+     */
+    DataGridView queryAllLoginfo(LoginfoVo loginfoVo);
 }

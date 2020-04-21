85b828201d0e725e4074df6c5b096e1c4188abb7
xhh <303158131@qq.com>
2020-04-21 12:15:10 +0800

    add NoticeController

--- erp_permissions/src/main/java/cn/ddossec/controller/NoticeController.java
+++ erp_permissions/src/main/java/cn/ddossec/controller/NoticeController.java
@@ -1,10 +1,19 @@
 package cn.ddossec.controller;
 
+import cn.ddossec.common.ActiveUser;
+import cn.ddossec.common.ResultObj;
+import cn.ddossec.domain.Notice;
 import cn.ddossec.service.NoticeService;
 import cn.ddossec.vo.NoticeVo;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * @author 30315
@@ -22,10 +31,33 @@ public class NoticeController {
 
     /**
      * 查询
+     *
+     * @param noticeVo
+     * @return
      */
-    @RequestMapping("loadAllNotice")
-    public Object loadAllNotice(NoticeVo noticeVo){
+    @GetMapping("loadAllNotice")
+    public Object loadAllNotice(NoticeVo noticeVo) {
         return this.noticeService.queryAllNotice(noticeVo);
+    }
+
+    /**
+     * 添加公告
+     * @param notice
+     * @return
+     */
+    @PostMapping("addNotice")
+    public ResultObj addNotice(Notice notice) {
+        try {
+            Subject subject = SecurityUtils.getSubject();
+            ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
+            notice.setOpername(activeUser.getUser().getName());
+            notice.setCreatetime(new Date());
+            this.noticeService.save(notice);
+            return ResultObj.ADD_SUCCESS;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResultObj.ADD_ERROR;
+        }
     }
 
 }

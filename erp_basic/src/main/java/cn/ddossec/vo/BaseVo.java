1976a2707b172d5b9a8bf2dc31831f8c3870aedb
xhh <303158131@qq.com>
2020-04-23 11:45:34 +0800

    add ProductTypeVo

--- erp_basic/src/main/java/cn/ddossec/vo/BaseVo.java
+++ erp_basic/src/main/java/cn/ddossec/vo/BaseVo.java
@@ -1,5 +1,9 @@
 package cn.ddossec.vo;
 
+import lombok.Data;
+
+import java.io.Serializable;
+
 /**
  * @author 30315
  * @title: BaseVo
@@ -7,6 +11,10 @@ package cn.ddossec.vo;
  * @description: TODO
  * @date 2020-04-2311:44
  */
+@Data
+public class BaseVo implements Serializable {
 
-public class BaseVo {
+    private Integer page;
+    private Integer pageSize;
+
 }

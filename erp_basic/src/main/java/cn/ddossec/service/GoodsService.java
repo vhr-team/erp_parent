0be7f15305264140a272cbfce1a3cd8ac97ab770
xhh <303158131@qq.com>
2020-04-24 10:21:04 +0800

    add 查询所有商品，可以带查询条件

--- erp_basic/src/main/java/cn/ddossec/service/GoodsService.java
+++ erp_basic/src/main/java/cn/ddossec/service/GoodsService.java
@@ -1,15 +1,23 @@
 package cn.ddossec.service;
 
+import cn.ddossec.common.DataGridView;
 import cn.ddossec.domain.Goods;
+import cn.ddossec.vo.GoodsVo;
 import com.baomidou.mybatisplus.extension.service.IService;
-    /**  
-    * @title: GoodsService
-    * @projectName erp_parent
-    * @description: TODO
-    * @author 30315
-    * @date 2020-04-2410:14
-    */
-public interface GoodsService extends IService<Goods>{
 
+/**
+ * @author 30315
+ * @title: GoodsService
+ * @projectName erp_parent
+ * @description: TODO
+ * @date 2020-04-2410:14
+ */
+public interface GoodsService extends IService<Goods> {
 
+    /**
+     * 查询所有商品，可以带查询条件
+     * @param goodsVo
+     * @return
+     */
+    DataGridView queryAllGoods(GoodsVo goodsVo);
 }

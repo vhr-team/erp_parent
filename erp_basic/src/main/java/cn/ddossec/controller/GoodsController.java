40528ec6cadbe6f7e8f6f838f1ec028c1caa1a38
xhh <303158131@qq.com>
2020-04-24 10:18:25 +0800

    create GoodsController

---
+++ erp_basic/src/main/java/cn/ddossec/controller/GoodsController.java
@@ -0,0 +1,11 @@
+package cn.ddossec.controller;
+
+/**
+ * @author 30315
+ * @title: GoodsController
+ * @projectName erp_parent
+ * @description: TODO
+ * @date 2020-04-2410:18
+ */
+public class GoodsController {
+}

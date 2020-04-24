1711fa5b678996c5a6507f02864f8bbcfa8a12cd
xhh <303158131@qq.com>
2020-04-24 10:21:15 +0800

    add goodsvo

---
+++ erp_basic/src/main/java/cn/ddossec/vo/GoodsVo.java
@@ -0,0 +1,15 @@
+package cn.ddossec.vo;
+
+import lombok.Data;
+
+/**
+ * @author 30315
+ * @title: ProductTypeVo
+ * @projectName erp_parent
+ * @description: TODO
+ * @date 2020-04-2311:45
+ */
+@Data
+public class GoodsVo extends BaseVo {
+
+}

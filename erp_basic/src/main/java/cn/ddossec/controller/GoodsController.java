26794e20ecce8c5983f64e3057af67e33a646650
xhh <303158131@qq.com>
2020-04-24 10:20:51 +0800

    add 查询所有商品，可以带查询条件

--- erp_basic/src/main/java/cn/ddossec/controller/GoodsController.java
+++ erp_basic/src/main/java/cn/ddossec/controller/GoodsController.java
@@ -1,5 +1,13 @@
 package cn.ddossec.controller;
 
+import cn.ddossec.common.DataGridView;
+import cn.ddossec.service.GoodsService;
+import cn.ddossec.vo.GoodsVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author 30315
  * @title: GoodsController
@@ -7,5 +15,22 @@ package cn.ddossec.controller;
  * @description: TODO
  * @date 2020-04-2410:18
  */
+@RequestMapping("goods")
+@RestController
 public class GoodsController {
+
+    @Autowired
+    private GoodsService goodsService;
+
+    /**
+     * 查询所有商品，可以带查询条件
+     *
+     * @param goodsVo
+     * @return
+     */
+    @GetMapping("queryAllGoods")
+    public DataGridView queryAllGoods(GoodsVo goodsVo) {
+        return this.goodsService.queryAllGoods(goodsVo);
+    }
+
 }

dd415c881877378887c6b02f983cbd04613dd5dc
xhh <303158131@qq.com>
2020-04-26 15:17:07 +0800

    add 订单查询条件的封装类

---
+++ erp_procurement/src/main/java/cn/ddossec/vo/OrderModelVo.java
@@ -0,0 +1,14 @@
+package cn.ddossec.vo;
+
+import lombok.*;
+
+/**
+ * 订单查询条件的封装类
+ */
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderModelVo extends BaseVo {
+}

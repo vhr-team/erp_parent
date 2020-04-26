73fe460f79d483f0d2f9aaf7e7947f243cc25e3b
xhh <303158131@qq.com>
2020-04-26 15:16:08 +0800

    add OrderModelController

--- erp_procurement/src/main/java/cn/ddossec/controller/OrderModelController.java
+++ erp_procurement/src/main/java/cn/ddossec/controller/OrderModelController.java
@@ -1,4 +1,15 @@
 package cn.ddossec.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 订单控制器
+ */
+@RestController
+@RequestMapping("ordermodel")
 public class OrderModelController {
+
+
+
 }

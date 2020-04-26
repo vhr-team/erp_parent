f9b4f101368b219a0c64f2b341b1f39ed4bc7fdf
xhh <303158131@qq.com>
2020-04-26 15:23:44 +0800

    add 查询所有订单，可以带条件，分页查询

--- erp_procurement/src/main/java/cn/ddossec/service/OrderModelService.java
+++ erp_procurement/src/main/java/cn/ddossec/service/OrderModelService.java
@@ -1,8 +1,16 @@
 package cn.ddossec.service;
 
+import cn.ddossec.common.DataGridView;
 import cn.ddossec.domain.OrderModel;
+import cn.ddossec.vo.OrderModelVo;
 import com.baomidou.mybatisplus.extension.service.IService;
-public interface OrderModelService extends IService<OrderModel>{
 
+public interface OrderModelService extends IService<OrderModel> {
 
+    /**
+     * 查询所有订单，可以带条件，分页查询
+     * @param orderModelVo
+     * @return
+     */
+    DataGridView queryAllOrderModel(OrderModelVo orderModelVo);
 }

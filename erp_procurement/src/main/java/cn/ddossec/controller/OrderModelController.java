b41979d1cee02e92ec1e3fbd7562ef53d979eb58
xhh <303158131@qq.com>
2020-04-26 15:22:39 +0800

    add 查询所有订单，可以带条件，分页查询

--- erp_procurement/src/main/java/cn/ddossec/controller/OrderModelController.java
+++ erp_procurement/src/main/java/cn/ddossec/controller/OrderModelController.java
@@ -1,5 +1,10 @@
 package cn.ddossec.controller;
 
+import cn.ddossec.common.DataGridView;
+import cn.ddossec.service.OrderModelService;
+import cn.ddossec.vo.OrderModelVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("ordermodel")
 public class OrderModelController {
 
+    @Autowired
+    private OrderModelService orderModelService;
 
+    /**
+     * 查询所有订单，可以带条件，分页查询
+     * @param orderModelVo
+     * @return
+     */
+    @GetMapping("queryAllOrderModel")
+    public DataGridView queryAllOrderModel(OrderModelVo orderModelVo){
+        return this.orderModelService.queryAllOrderModel(orderModelVo);
+    }
 
 }

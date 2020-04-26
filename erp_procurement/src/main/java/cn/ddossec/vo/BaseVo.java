2dc64ff7b138144aef38f711a271e06c8b2bb1d0
xhh <303158131@qq.com>
2020-04-26 15:15:58 +0800

    add BaseVo

---
+++ erp_procurement/src/main/java/cn/ddossec/vo/BaseVo.java
@@ -0,0 +1,16 @@
+package cn.ddossec.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class BaseVo implements Serializable {
+
+    // 当前页
+    private Integer page;
+
+    // 显示条数
+    private Integer pageSize;
+
+}

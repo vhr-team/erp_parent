e927a1a5130016da5b0b10b27746f000db9f67bf
xhh <303158131@qq.com>
2020-04-26 14:19:03 +0800

    添加启动类注解

--- erp_procurement/src/main/java/cn/ddossec/ProcurementApp.java
+++ erp_procurement/src/main/java/cn/ddossec/ProcurementApp.java
@@ -1,9 +1,17 @@
 package cn.ddossec;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * 采购模块
  * 唐疑豪
  */
-
+// 启动类注解
+@SpringBootApplication
 public class ProcurementApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcurementApp.class, args);
+    }
 }

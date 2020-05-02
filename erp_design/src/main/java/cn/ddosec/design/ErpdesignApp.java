9624d729b1ce429694986dd72c87949edaca1baa
joker_dj <joker_dingjian@qq.com>
2020-05-02 23:58:01 +0800

    服务注册，配置文件上传

--- erp_design/src/main/java/cn/ddosec/design/ErpdesignApp.java
+++ erp_design/src/main/java/cn/ddosec/design/ErpdesignApp.java
@@ -3,12 +3,15 @@ package cn.ddosec.design;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author joker_dj
  * @create 2020-04-30日 10:18
  */
 @SpringBootApplication
+// 服务发现
+@EnableDiscoveryClient
 @MapperScan("cn.ddosec.design.mapper")
 public class ErpdesignApp {
     public static void main(String[] args) {
